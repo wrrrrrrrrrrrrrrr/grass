3ee08d1e15ee6291c45cd9cef21a76bb37346b9c
unknown <cryptoleo1995@gmail.com>
2024-05-12 21:01:19 +0800

    debug

--- core/grass.py
+++ core/grass.py
@@ -163,7 +163,7 @@ class Grass(GrassWs, GrassRest, FailureCounter):
         while True:
             try:
                 await self.connection_handler()
-                await self.auth_to_extension(browser_id, user_id)
+                # await self.auth_to_extension(browser_id, user_id)
                 await self.connect_to_wss(self.wss_proxy, user_id)
 
                 # if self.proxy_score is None:
